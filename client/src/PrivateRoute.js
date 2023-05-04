26c62df55f955195423f208df7ef51c9746e015b
Vasikaran <vasikaran@p2fsemi.in>
2023-05-04 15:11:44 +0530

    test1

--- client/src/PrivateRoute.js
+++ client/src/PrivateRoute.js
@@ -4,6 +4,7 @@ import { Navigate, Outlet } from "react-router-dom";
 const PrivateRoute = ({ component: component, ...rest }) => {
   const token = localStorage.getItem("token");
   return token ? <Outlet /> : <Navigate to="/login" />;
+                  
 };
 
 export default PrivateRoute;
